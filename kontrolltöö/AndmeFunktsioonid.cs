aeb2d7d7240c6d736d738600d0abdda8dd1e7126
Kirill Fedulin <crazzyhd22@gmail.com>
2026-04-16 22:35:53 +0300

    Update AndmeFunktsioonid.cs

--- kontrolltöö/AndmeFunktsioonid.cs
+++ kontrolltöö/AndmeFunktsioonid.cs
@@ -1,95 +1,121 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
-using System.Formats.Tar;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.IO;
 using System.Text;
-using System.Transactions;
-using System.Xml.Schema;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace kontrolltöö
 {
-    internal class AndmeFunktsioonid
+    public class Auto
     {
-        public static void KirjutaLogi()
+        public string Mudel { get; set; }
+        public double KutuseKulu { get; set; }
+        public double PaagisOnKutust { get; set; }
+
+        public Auto(string mudel, double kutuseKulu, double paagisOnKutust)
         {
-            string retseptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logi.txt");
-
-            if (File.Exists(retseptPath))
-            {
-
-            }
+            Mudel = mudel;
+            KutuseKulu = kutuseKulu;
+            PaagisOnKutust = paagisOnKutust;
+        }
+        public double ArvutaSoiduulatus()
+        {
+            double kogus = (PaagisOnKutust / KutuseKulu) * 100;
+            Console.WriteLine("Soiduulatus: " + kogus);
+            return kogus;
         }
 
-        public static void RiigiOtsing()
+        public static void HaldaAutosid()
         {
-            Dictionary<string, string> Riigid = new Dictionary<string, string>();
-            Riigid.Add("EE", "Eesti");
-            Riigid.Add("FI", "Soome");
-            Console.WriteLine("Palun sissesta Riigikood: ");
-            string riik = Console.ReadLine();
-            foreach (KeyValuePair<string, string> paar in Riigid)
+            List<Auto> auto = new List<Auto>()
+        {
+            new Auto("Toyota", 5.5, 40),
+            new Auto("BMW", 8.0, 15),
+            new Auto("Audi", 6.5, 8)
+        };
+            Auto parim = auto.OrderByDescending(a => a.ArvutaSoiduulatus()).First();
+            Console.WriteLine("Suurima sõiduulatusega auto: " + parim.Mudel);
+
+            var vajaTankida = auto.Where(a => a.PaagisOnKutust < 10);
+            Console.WriteLine("Vajavad tankimist:");
+            foreach (var a in vajaTankida)
             {
-                if(paar.Key == riik)
+                Console.WriteLine(a.Mudel);
+            }
+
+  
+            using (StreamWriter sw = new StreamWriter("autod.txt"))
+            {
+                foreach (var a in auto)
                 {
-                    Console.WriteLine("Riik on:" + paar.Value);
-                    break;
-                }
-                foreach (KeyValuePair<string, string> paar2 in Riigid)
-                {
-                    if (paar2.Key != riik)
-                    {
-                        Console.WriteLine("Meil ei ole see riik!");
-                        Console.WriteLine("Mis riik see on?: ");
-                        string vastus = Console.ReadLine();
-                        Riigid.Add(riik, vastus);
-                    }
-                }
-
-
+                    sw.WriteLine($"{a.Mudel},{a.KutuseKulu},{a.PaagisOnKutust}");
                 }
             }
-        
 
-        public class Auto()
-        {
-            public string Mudel { get; set; }
-            public double KutuseKulu { get; set; }
-            public double PaagisOnKutust { get; set; }
-
-            public static double ArvutaSoiduulatus(double KutuseKulu, double PaagisOnKutust)
-            {
-                double vastus = (PaagisOnKutust / KutuseKulu * 100);
-                return vastus;
-
-            }
-
-        }
-
-
-
-
-
-       public static void LoeJaArvuta()
-       {
-                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "arvud.txt");
-                StreamReader numbrid = new StreamReader(path);
-                string laused = numbrid.ReadToEnd();
-                laused.Split(",");
-                numbrid.Close();
-        }
-
-        public static void AndmeFunktsioonidd()
-        {
-
-        }
-
-        public static void AndmeFunktsjonijiet()
-        {
-
-        }
 
         }
     }
+    public class AndmeFunktsioonid
+    {
+        public static void KirjutaLogi(string kasutaja)
+        {
+            string path = @"..\..\..\logi.txt";
+            string date = DateTime.Now.ToString();
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine($"{date} - {kasutaja}");
+            }
+        }
+        public static void RiigiOtsing(Dictionary<string, string> riigid)
+        {
+            Console.Write("sisesta riik:  ");
+            string riik = Console.ReadLine().ToUpper();
+
+            if (riigid.ContainsKey(riik))
+            {
+                Console.WriteLine("Riik: " + riigid[riik]);
+            }
+            else
+            {
+                Console.Write("Riiki ei leitud. Sisesta riigi nimi: ");
+
+            }
+
+        }
+
+        public static Tuple<int, double> LoeJaArvuta()
+        {
+            try
+            {
+                string text = File.ReadAllText("arvud.txt");
+                string[] osad = text.Split(',');
+
+                int summa = 0;
+                foreach (string s in osad)
+                {
+                    summa += int.Parse(s);
+                }
+
+                double keskmine = summa / osad.Length;
+                Console.WriteLine($"summa: {summa}; keakmine: {Math.Round(keskmine), 2}");
+                return new Tuple<int, double>(summa, keskmine);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Viga: " + e);
+                return new Tuple<int, double>(0, 0);
+            }
+        }
+       
+
+
+
+    }
+
+    
+}
+    
